9b755d4905699f006d2aef14c24ee7c532f9c208
Carlos Alba <soradev4@gmail.com>
2020-12-26 18:01:37 -0800

    Basic Login Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Box, Button, Heading } from 'grommet';
+import { Github } from 'grommet-icons';
 
 function App() {
+
+  function githubLoginHandler() {
+    alert('Logging in!')
+  }
+
   return (
-    <h1>header file</h1>
+    <Box align="center" justify="center">
+      <Heading>Welcome to the Hack Tool!</Heading>
+      <Button onClick={githubLoginHandler} primary label="Continue with Github" icon={<Github />} />
+    </Box>
   );
 }
 
