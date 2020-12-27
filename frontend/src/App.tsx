6de0fd831c9feda457df7fc27f31f8659bd199c9
Carlos Alba <soradev4@gmail.com>
2020-12-27 00:38:48 -0800

    login redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Box, Button, Heading } from 'grommet';
 import { Github } from 'grommet-icons';
+import { useRecoilValue } from 'recoil';
+import { currentUserAuthTokenState } from './AppAtoms';
+import { useHistory } from 'react-router-dom';
 
 function App() {
+
+  const currentToken = useRecoilValue(currentUserAuthTokenState);
+  const history = useHistory();
+
+  const checkForToken = useCallback(() => {
+    if (currentToken && currentToken.length >= 1) {
+      history.push('/dashboard');
+    }
+  }, [currentToken, history])
+
+  useEffect(() => {
+    checkForToken();
+  }, [checkForToken])
+
   function githubLoginHandler() {
     window.location.replace(`${process.env.REACT_APP_API_URL}/github/login`)
   }
